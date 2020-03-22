b4bec1dd463f7ccd944e413b983ae37e9c89325e
BARIS GUNENC <baris.gunenc00@hotmail.com>
2020-03-23 00:18:14 +0300

    Updates

---
+++ precache-manifest.fd3c36be6b290aac64ecaeb44bf20bfe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4fd6364aa5f84dafa4fc9d9724fc95",
+    "url": "/punipuni-site/index.html"
+  },
+  {
+    "revision": "a1be6c5521a4e24aef78",
+    "url": "/punipuni-site/static/css/2.2574bb7b.chunk.css"
+  },
+  {
+    "revision": "83f6eb270b422f1d7086",
+    "url": "/punipuni-site/static/css/main.b06fd8cb.chunk.css"
+  },
+  {
+    "revision": "a1be6c5521a4e24aef78",
+    "url": "/punipuni-site/static/js/2.e1dd743b.chunk.js"
+  },
+  {
+    "revision": "9e3f40ea04bbb9fe4c222c108facde66",
+    "url": "/punipuni-site/static/js/2.e1dd743b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f6eb270b422f1d7086",
+    "url": "/punipuni-site/static/js/main.8afe5c90.chunk.js"
+  },
+  {
+    "revision": "a18808c6c70322288931",
+    "url": "/punipuni-site/static/js/runtime-main.1608922e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/punipuni-site/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/punipuni-site/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/punipuni-site/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/punipuni-site/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/punipuni-site/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ce24218259e310e19f4ff4b34b2c9622",
+    "url": "/punipuni-site/static/media/yut-cov2.ce242182.png"
+  }
+]);
