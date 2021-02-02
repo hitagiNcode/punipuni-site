5e4d324738fb95ce5e7355b2d58cb94b3a6defb8
Barış Günenç <power.of.baris@gmail.com>
2021-02-02 23:58:34 +0300

    adding scary family page

--- src/pages/FromhellPage.js
+++ src/pages/FromhellPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function FromhellPage() {
-  return <div className="container-fluid">This is scary family page.</div>;
+  return <div className="b-background container text-center">
+    <h1 className="h2 text-center p-3">Scary Family 3D - Prank Simulator</h1>
+    
+    
+    
+    
+    </div>;
 }
 
 export default FromhellPage;
